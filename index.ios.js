e3b6827006ea17c97a2765071fd0241c0003e007
cowlinator <cowlinator@users.noreply.github.com>
2017-05-07 17:30:44 -0700

    added disclaimer to ios version

--- index.ios.js
+++ index.ios.js
@@ -17,14 +17,7 @@ export default class RegexDownloader extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          This application is supported only on Android.
         </Text>
       </View>
     );
@@ -42,11 +35,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
